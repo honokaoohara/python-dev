0b2b925a092713bc7c11f6d22ac53fa9be49050c
taiking <c.tsujiyan727@gmail.com>
2020-12-01 14:57:01 +0900

    postも

--- server.py
+++ server.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return jsonify({"language": "python"})
+    return jsonify({"status": "200"})
+
+@app.route("/post", methods=['POST'])
+def post():
+    return jsonify({"status": "204"})
 
 if __name__ == "__main__":
     app.run(port=8080,host='0.0.0.0')
