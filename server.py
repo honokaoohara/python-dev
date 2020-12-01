7ce50d972af18d96feddc1b03a45c49eec7c344f
taiking <c.tsujiyan727@gmail.com>
2020-12-01 15:20:30 +0900

    fix

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ def index():
 
 @app.route("/post", methods=['POST'])
 def post():
-    return jsonify({"status": "204"})
+    return jsonify({"status": "201"})
 
 if __name__ == "__main__":
     app.run(port=8080,host='0.0.0.0')
