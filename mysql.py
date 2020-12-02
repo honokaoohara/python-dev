faebe8e1a85a2d47382c825c46473f9da60b3f7b
taiking <c.tsujiyan727@gmail.com>
2020-12-02 17:37:51 +0900

    make api

--- mysql.py
+++ mysql.py
@@ -1,12 +1,18 @@
 from flask import *
 from flask_cors import CORS
+import MySQLdb
 
 app = Flask(__name__)
 CORS(app)
 
+connection = MySQLdb.connect(user='root', passwd='', host='localhost', db='app', charset="utf8")
+
 @app.route("/tweets")
 def index():
-    return jsonify({"status": "200"})
+    cursor = connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute("select * from tweets")
+    users = cursor.fetchall()
+    return jsonify(users)
 
 if __name__ == "__main__":
     app.run(port=8080,host='0.0.0.0')
